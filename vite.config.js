dbbe6b6c7e61e08675d90438ee7b236bd94e7995
Always Ritz <161363790+Ritz404@users.noreply.github.com>
2025-01-18 16:22:08 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://wan-anime-stream.vercel.app',
+        target: 'https://wan-anime-strea.vercel.app',
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, ''),
